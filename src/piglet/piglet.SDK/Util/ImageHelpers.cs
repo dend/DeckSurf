3c588600b86652156c069d179f625060a1b79f6e
Den Delimarsky <1389609+dend@users.noreply.github.com>
2021-06-08 21:37:54 -0700

    Image helper

---
+++ src/piglet/piglet.SDK/Util/ImageHelpers.cs
@@ -0,0 +1,45 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace piglet.SDK.Util
+{
+    public class ImageHelpers
+    {
+        public static byte[] ResizeImage(string path, int width, int height)
+        {
+            if (File.Exists(path))
+            {
+                Image currentImage = Image.FromFile(path);
+
+                var targetRectangle = new Rectangle(0, 0, width, height);
+                var targetImage = new Bitmap(width, height);
+
+                targetImage.SetResolution(currentImage.HorizontalResolution, currentImage.VerticalResolution);
+                
+                using(var graphics = Graphics.FromImage(targetImage))
+                {
+                    graphics.CompositingMode = CompositingMode.SourceCopy;
+                    graphics.CompositingQuality = CompositingQuality.HighQuality;
+                    graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                    graphics.SmoothingMode = SmoothingMode.HighQuality;
+                    graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                    using (var wrapMode = new ImageAttributes())
+                    {
+                        wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                        graphics.DrawImage(currentImage, targetRectangle, 0, 0, currentImage.Width, currentImage.Height, GraphicsUnit.Pixel, wrapMode);
+                    }
+                }
+
+                var converter = new ImageConverter();
+                return (byte[])converter.ConvertTo(targetImage, typeof(byte[]));
+            }
+            else
+            {
+                throw new FileNotFoundException("File not found. Make sure the path is correct.");
+            }
+        }
+    }
+}
